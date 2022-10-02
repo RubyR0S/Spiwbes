a4a97e65092f13384831416e9b216267026e3c38
Ros <Fedorovch>
2022-10-02 20:08:31 +0300

    Created Admin::UsersController

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,38 @@
+class Admin::UsersController < ApplicationController
+  before_action :find_user, except: :index
+  def index
+    @users = User.order(created_at: :desc)
+  end
+
+  def show
+  end 
+
+  def edit
+  end
+
+  def update
+    if @user.update user_params
+      flash[:success] = 'User updated!'
+      redirect_to admin_users_path
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = 'Question deleted!'
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def find_user
+    @user = User.find_by id: params[:id]
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email)
+  end
+end
+
