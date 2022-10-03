8b694bf5e30a98ef200faa0ca39587db9240a38d
Ros <Fedorovch>
2022-10-03 15:02:39 +0300

    Deletd Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+ 
   resources :admin, only: %i[new create]
 
   namespace :admin do
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_02_160742) do
-  create_table "admins", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2022_10_01_172048) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
