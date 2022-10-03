a83df158efdf45ecf862ba12463a01964d9eaa97
Ros <Fedorovch>
2022-10-03 17:32:40 +0300

    Added method 'current_admin' and 'admin_signed_in'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  private
+  
+  def current_admin
+    @current_admin ||= Admin.find_by(id: session[:admin_id]) if session[:admin_id].present? 
+  end
+
+  def admin_signed_in?
+    current_admin.present?
+  end
+
+  helper_method :current_admin, :admin_signed_in?
 end
