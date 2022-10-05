65443b0cdfbed040156175f242c635bf94761642
Ros <Fedorovch>
2022-10-05 21:40:34 +0300

    Small changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,15 @@ class UsersController < ApiController
   before_action :authorize_request, except: %i[create update]
   before_action :find_user, except: %i[create index]
 
-  # GET /users
   def index
     @users = User.all
     render json: @users, status: :ok
   end
 
-  # GET /users/{username}
   def show
     render json: @user, status: :ok
   end
 
-  # POST /users
   def create
     @user = User.new(user_params)
     if @user.save
@@ -26,23 +23,10 @@ class UsersController < ApiController
     end
   end
 
-  # PUT /users/{username}
-  def update
-    unless @user.update(user_params)
-      render json: { errors: @user.errors.full_messages },
-             status: :unprocessable_entity#
-    end
-  end
-
-  # DELETE /users/{username}
-  def destroy
-    @user.destroy
-  end
-
   private
 
   def find_user
-    @user = User.find_by_username!(params[:_username])
+    @user = User.find params[:id]
     rescue ActiveRecord::RecordNotFound
       render json: { errors: 'User not found' }, status: :not_found
   end
