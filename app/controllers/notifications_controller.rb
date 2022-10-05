09db0e50cb13bb4ade73bc10e14f884452d33aeb
Ros <Fedorovch>
2022-10-05 22:03:39 +0300

    Created NotificationsController

---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,25 @@
+class NotificationsController < ApiController
+before_action :authorize_request
+
+  def show
+    render json: @notification, status: :ok
+  end
+
+  def create
+    @notification = @current_user.notifications.new(notification_params)
+    if @notification.save
+      render json: @notification, status: :ok
+    else
+      render json: { errors: @notification.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def notification_params
+    params.permit(
+      :title, :body
+    )
+  end
+end
